843782ac37b0f940cbc6cba9a62876ab7c3fa594
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-11-20 18:46:25 -0300

    Criando camada adpter para a busca por id
    Adpter criando para buscar o customer no banco de dados

--- hexagonal/src/main/java/com/gabryel/hexagonal/adapter/out/FindAddressByZipCodeAdapter.java
+++ hexagonal/src/main/java/com/gabryel/hexagonal/adapter/out/FindAddressByZipCodeAdapter.java
@@ -4,18 +4,15 @@ import com.gabryel.hexagonal.adapter.out.client.FindAddressByZipCodeClient;
 import com.gabryel.hexagonal.adapter.out.client.mapper.AddressMapper;
 import com.gabryel.hexagonal.application.core.domain.Address;
 import com.gabryel.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class FindAddressByZipCodeAdapter implements FindAddressByZipCodeOutputPort {
 
     private final FindAddressByZipCodeClient clientZipCode;
     private final AddressMapper addressMapper;
-    
-    public FindAddressByZipCodeAdapter(FindAddressByZipCodeClient clientZipCode, AddressMapper addressMapper) {
-        this.clientZipCode = clientZipCode;
-        this.addressMapper = addressMapper;
-    }
 
     @Override
     public Address find(String zipCode) {
--- hexagonal/src/main/java/com/gabryel/hexagonal/adapter/out/FindCustomerByIdAdapter.java
+++ hexagonal/src/main/java/com/gabryel/hexagonal/adapter/out/FindCustomerByIdAdapter.java
@@ -1,21 +1,25 @@
 package com.gabryel.hexagonal.adapter.out;
 
+import com.gabryel.hexagonal.adapter.in.controller.mapper.CustomerMapper;
 import com.gabryel.hexagonal.adapter.out.repository.CustomerRepository;
 import com.gabryel.hexagonal.adapter.out.repository.mapper.CustomerEntityMapper;
 import com.gabryel.hexagonal.application.core.domain.Customer;
-import com.gabryel.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import com.gabryel.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
-public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+public class FindCustomerByIdAdapter implements FindCustomerByIdOutputPort {
 
-    private final CustomerEntityMapper customerEntityMapper;
     private final CustomerRepository customerRepository;
+    private final CustomerEntityMapper customerEntityMapper;
 
     @Override
-    public void insert(Customer customer) {
-        customerRepository.save(customerEntityMapper.toEntity(customer));
+    public Optional<Customer> find(String id) {
+
+        return customerRepository.findById(id).map(customerEntityMapper::toDomain);
     }
 }
