b032742c478c07e264225bc1f852ff9cafb9f3c1
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-11-24 21:02:15 -0300

    Configurando os acessos das camadas
    Teste necessário para garantia da arquitetura para qualquer nova implementação

--- hexagonal/src/test/java/com/gabryel/hexagonal/architecture/LayeredArchitectureTest.java
+++ hexagonal/src/test/java/com/gabryel/hexagonal/architecture/LayeredArchitectureTest.java
@@ -1,4 +1,33 @@
 package com.gabryel.hexagonal.architecture;
 
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+@AnalyzeClasses(packages = "com.gabryel.hexagonal")
+@SpringBootTest
 public class LayeredArchitectureTest {
+
+    @ArchTest
+    public static final ArchRule layered_architecture_test = layeredArchitecture().consideringAllDependencies()
+
+            // Define layers
+            .layer("AdaptersIn").definedBy("..adapter.in..")
+            .layer("AdaptersOut").definedBy("..adapter.out..")
+            .layer("UseCase").definedBy("..application.core.usecase..")
+            .layer("PortsIn").definedBy("..application.ports.in..")
+            .layer("PortsOut").definedBy("..application.ports.out..")
+            .layer("Configuration").definedBy("..configuration..")
+
+            // Define dependencies
+            .whereLayer("AdaptersIn").mayOnlyBeAccessedByLayers("Configuration")
+            .whereLayer("AdaptersOut").mayOnlyBeAccessedByLayers("Configuration")
+            .whereLayer("UseCase").mayOnlyBeAccessedByLayers("Configuration")
+            .whereLayer("PortsIn").mayOnlyBeAccessedByLayers("UseCase", "AdaptersIn")
+            .whereLayer("PortsOut").mayOnlyBeAccessedByLayers("UseCase", "AdaptersOut")
+            .whereLayer("Configuration").mayNotBeAccessedByAnyLayer();
+
 }
